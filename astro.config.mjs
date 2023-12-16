ca25056a25d88e27bc1c475674d4e01e0fa75fc7
Atsushi Nagaki <atsushi@nagaki.me>
2023-12-16 22:34:22 +0900

    Add redirects for paint pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,4 +13,13 @@ export default defineConfig({
   },
   integrations: [mdx()],
   site: "https://nagaki.github.io",
+  redirects: {
+    "/paint/square": "/paint/2007-05-19-square",
+    "/paint/kao": "/paint/2007-08-07-kao",
+    "/paint/pet": "/paint/2007-10-05-pet",
+    "/paint/line": "/paint/2007-11-07-line",
+    "/paint/tegaki": "/paint/2008-01-16-tegaki",
+    "/paint/signboard": "/paint/2008-01-20-signboard",
+    "/paint/pencil": "/paint/2008-03-03-pencil",
+  },
 })
