58d4a3c7f7d6c85841401702f22518d82d4b0606
Atsushi Nagaki <atsushi@nagaki.me>
2025-06-29 01:17:23 +0900

    fix feed.xml content issue

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -7,21 +7,42 @@ import sanitizeHtml from "sanitize-html";
 const parser = new MarkdownIt();
 
 export async function GET(context) {
-	const allDiaries = await getCollection("diary");
-	const allPaints = await getCollection("paint");
-	const allPractices = await getCollection("practice");
-	const posts = combinePosts([...allDiaries, ...allPaints, ...allPractices]);
-	return rss({
-		title: "ALLAURMONO",
-		description: "ひびのできごとなどをたまにかきます",
-		site: context.site,
-		items: posts.map(({ slug, body, collection, date, data }) => {
-			return {
-				title: data.title,
-				pubDate: date,
-				content: sanitizeHtml(parser.render(body)),
-				link: `/${collection}/${slug}.html`,
-			};
-		}),
-	});
+  const allDiaries = await getCollection("diary");
+  const allPaints = await getCollection("paint");
+  const allPractices = await getCollection("practice");
+  const posts = combinePosts([...allDiaries, ...allPaints, ...allPractices]);
+  return rss({
+    title: "ALLAURMONO",
+    description: "ひびのできごとなどをたまにかきます",
+    site: context.site,
+    items: posts.map(({ slug, body, collection, date, data }) => {
+      return {
+        title: data.title,
+        pubDate: date,
+        content:
+          // TODO: 画像のURLを絶対パスにする
+          collection === "paint"
+            ? sanitizeHtml(
+                data.images
+                  .map(
+                    (image) =>
+                      `<img src="/img/uploads/${data.directory}/${image}" />`,
+                  )
+                  .join(""),
+                {
+                  allowedTags: sanitizeHtml.defaults.allowedTags.concat([
+                    "img",
+                  ]),
+                },
+              )
+            : sanitizeHtml(parser.render(body), {
+                allowedTags: sanitizeHtml.defaults.allowedTags.concat([
+                  "img",
+                  "time",
+                ]),
+              }),
+        link: `/${collection}/${slug}.html`,
+      };
+    }),
+  });
 }
